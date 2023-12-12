aee1a285141b15fccca3dd50f3566f51ac794401
Muhammad Ilham Karisma <ilhamsssa@gmail.com>
2023-12-12 11:31:11 +0700

    feat: init router

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const route = require('./src/route');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.set("view engine", "ejs");
+
+app.use(route);
 
 const listener = app.listen(process.env.PORT || 3000, async () => {
   console.log(`Server is running on port ${listener.address().port}`);
